270c33935bd0f2f4f658f8345e090a65bdf0b769
Carlos Edril <ruzey.el@gmail.com>
2024-05-02 05:44:02 -0300

    correção para deixar o saldo fixo

---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+	
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite o número da sua conta");
+        int conta = scanner.nextInt();
+
+        System.out.println("Agora, o número da sua agência");
+        int agencia = scanner.nextInt();
+        
+        System.out.println("Informe qual seu saldo atual");
+        double saldo = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco"
+        		+ ", sua agência é " + agencia + ", conta " 
+        		+ conta + " e seu saldo de R$" 
+        		+ saldo + " já está disponível para saque.");
+    }
+}
